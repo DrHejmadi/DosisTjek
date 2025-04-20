73a77e96d36350dd7e97dd8e1f1c8239e53e9ac0
Michael Hejmadi <hejmadi@gmail.com>
2025-04-20 23:28:32 +0200

    Initial Commit

---
+++ DosisTjek/DosisTjekApp.swift
@@ -0,0 +1,17 @@
+//
+//  DosisTjekApp.swift
+//  DosisTjek
+//
+//  Created by Michael Hejmadi on 20/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct DosisTjekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
